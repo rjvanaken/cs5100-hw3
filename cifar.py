98644a3b38468818eee454c71bc525896e2a9792
Rebecca Van Aken <rjvanaken@gmail.com>
2026-04-04 11:46:21 -0400

    model 1 - FF

--- cifar.py
+++ cifar.py
@@ -41,9 +41,30 @@ device = (
     
 
 # Define model
-class NeuralNetwork(nn.Module):
+# model 1 (feed forward)
+class NeuralNetworkFF (nn.Module):
     def __init__(self):
         super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential (
+            nn.Linear(32*32*3, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+
+        )
+
+    def forward(self, x):
+      x = self.flatten(x)
+      logits = self.linear_relu_stack(x)
+      return logits
+
+
+
+
 
 
 # continue...
