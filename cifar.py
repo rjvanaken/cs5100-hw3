593dea59c1559dd89cae8033b629bab36c2c2aa5
Rebecca Van Aken <rjvanaken@gmail.com>
2026-04-03 23:00:46 -0400

    download training and test data and create data loaders

--- cifar.py
+++ cifar.py
@@ -1,10 +1,44 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+from torchvision.transforms import ToTensor
 
 
 
 
 
 def main():
-    pass
+
+    # download training data
+    training_data = datasets.CIFAR10(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+    
+    # download test data
+    test_data = datasets.CIFAR10(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor()
+    )
+
+    batch_size = 64
+
+    # create data loaders
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+
+
+
+
+
+    
 
 
 if __name__ == "__main__":
