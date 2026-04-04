00a377deaf58b55fafca4f7aa582189569072349
rjvanaken <rjvanaken@gmail.com>
2026-04-04 15:41:36 -0400

    test and train functions, run function for looping epochs

--- cifar.py
+++ cifar.py
@@ -64,7 +64,6 @@ class FeedForward (nn.Module):
       return logits
     
 
-
 # model 2 (feed forward with Tanh)
 class FeedForwardTanh (nn.Module): # if I change this up enough may need to rename
     def __init__(self):
@@ -108,9 +107,86 @@ class ConvolutionalNetwork (nn.Module):
         return logits
 
 
+model_ff = FeedForward().to(device)
+model_fft = FeedForwardTanh().to(device)
+model_cnn = FeedForwardTanh().to(device)
 
 
-# continue...
+
+# TRAIN AND TEST
+
+
+
+# train model
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        #prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        #backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+# test model
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        
+        accuracy = correct / size
+        print(f"accuracy: {accuracy}\n")
+
+# RUN
+# ------------------
+# model 1
+
+#constants 
+LOSS_FN = nn.CrossEntropyLoss()
+EPOCHS = 35
+
+def run(model):
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    for t in range(EPOCHS):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, LOSS_FN, optimizer)
+        test(test_dataloader, model, LOSS_FN)
+    print("Done!")
+
+
+
+def main():
+    run(model_ff)
+    # run(model_fft)
+    # run(model_cnn)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
 
